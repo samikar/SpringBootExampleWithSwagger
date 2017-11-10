a36d4992724955d9570a8cafaf7dc7a2b1f66b9b
Sami Karkkainen <e1400500@shell.vamk.fi>
2017-11-10 18:45:47 +0200

    Updated controllers

--- src/main/java/se/MunicipalityController.java
+++ src/main/java/se/MunicipalityController.java
@@ -14,29 +14,29 @@ public class MunicipalityController {
 	@Autowired
 	private MunicipalityRepository repository;
     
-    @RequestMapping("/municipalities")
-    public Iterable<Municipality> municipalities(@RequestParam(value="name", defaultValue="World") String name) {
-        return repository.findAll();
-    }
-    
-    @RequestMapping(value = "/municipality" , method = RequestMethod.POST)
-    public @ResponseBody Municipality addProject(@RequestBody Municipality jsonProject) {
-      return repository.save(jsonProject);
-    }
-    
-    @RequestMapping("/municipality/{id}")
-    public Municipality getMunicipality(@PathVariable("id") int id) {
-    return repository.findOne(id);
-    }
-    
-    @RequestMapping(value = "/municipality" , method = RequestMethod.PUT)  //U
-    public @ResponseBody Municipality update(@RequestBody Municipality item) {
-    return repository.save(item);
-    } 
-    
-    @RequestMapping(value = "/municipality" , method = RequestMethod.DELETE)  //D
-    public void delete(@RequestBody Municipality item) {
-    repository.delete(item);
-    //do business logic
-    }
+	@RequestMapping(value = "/municipality" , method = RequestMethod.POST)
+	 public @ResponseBody Municipality create(@RequestBody Municipality item) {    //C
+	 return repository.save(item);
+	 } 
+	 
+	 @RequestMapping("/municipalities")
+	 public Iterable<Municipality> municipalities(@RequestParam(value="name", defaultValue="World") String name) { //R
+	 return repository.findAll();
+	 } 
+	 
+	 @RequestMapping("/municipality/{id}")
+	 public Municipality get(@PathVariable("id") int id) {   //R
+	 return repository.findOne(id);
+	 }
+
+	@RequestMapping(value = "/municipality" , method = RequestMethod.PUT)  //U
+	 public @ResponseBody Municipality update(@RequestBody Municipality item) {
+	 return repository.save(item);
+	 } 
+	 
+	 @RequestMapping(value = "/municipality" , method = RequestMethod.DELETE)  //D
+	 public void delete(@RequestBody Municipality item) {
+	 repository.delete(item);
+	 //do business logic
+	 }
 }
--- src/main/java/se/SensordataController.java
+++ src/main/java/se/SensordataController.java
@@ -1,6 +1,7 @@
 package se;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,19 +14,29 @@ public class SensordataController {
 	@Autowired
 	private SensordataRepository repository;
     
-    @RequestMapping("/sensordatas")
-    public Iterable<Sensordata> sensordatas(@RequestParam(value="name", defaultValue="World") String name) {
-        return repository.findAll();
-    }
-    
-    @RequestMapping(value = "/sensordatas" , method = RequestMethod.PUT)  //U
-    public @ResponseBody Sensordata update(@RequestBody Sensordata item) {
-    return repository.save(item);
-    } 
-    
-    @RequestMapping(value = "/sensordatas" , method = RequestMethod.DELETE)  //D
-    public void delete(@RequestBody Sensordata item) {
-    repository.delete(item);
-    //do business logic
-    }
+	@RequestMapping(value = "/sensordata" , method = RequestMethod.POST)
+	 public @ResponseBody Sensordata create(@RequestBody Sensordata item) {    //C
+	 return repository.save(item);
+	 } 
+	 
+	 @RequestMapping("/sensordatas")
+	 public Iterable<Sensordata> sensordatas(@RequestParam(value="name", defaultValue="World") String name) { //R
+	 return repository.findAll();
+	 } 
+	 
+	 @RequestMapping("/sensordata/{id}")
+	 public Sensordata get(@PathVariable("id") int id) {   //R
+	 return repository.findOne(id);
+	 }
+
+	@RequestMapping(value = "/sensordata" , method = RequestMethod.PUT)  //U
+	 public @ResponseBody Sensordata update(@RequestBody Sensordata item) {
+	 return repository.save(item);
+	 } 
+	 
+	 @RequestMapping(value = "/sensordata" , method = RequestMethod.DELETE)  //D
+	 public void delete(@RequestBody Sensordata item) {
+	 repository.delete(item);
+	 //do business logic
+	 }
 }
